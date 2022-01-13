55e030faab4651ae833130d2db01a120e5119f59
HongyouZhou <hongyou.chou@gmail.com>
2022-01-13 01:00:02 +0100

    Setup node.

--- main.py
+++ main.py
@@ -1,53 +1,164 @@
 import numpy as np
 import pybullet as p
+import networkx as nx
 from classic_framework.pybullet.PyBulletRobot import PyBulletRobot as Robot
 from classic_framework.pybullet.PyBulletScene import PyBulletScene as Scene
 from classic_framework.interface.Logger import RobotPlotFlags
 from classic_framework.pybullet.pb_utils.pybullet_scene_object import PyBulletObject
 
-maze_grid = [[0, 0, 0, 0],
-             [0, 0, 0, 0],
+MAZE_GRID = [[0, 1, 1, 0],
              [0, 1, 1, 0],
-             [0, 1, 1, 0]]
+             [0, 0, 0, 0],
+             [0, 0, 0, 0]]
+robot_grid_pos = [0, 0]
 
-maze_pos = [0.5, -0.1, 0.91]
-cs1_offset = [0.15, -0.06, 0.02]
-x_step_size = -0.04
-y_step_size = 0.04
-maze_origin_offset = np.array(maze_pos) + np.array(cs1_offset)
+MAZE_POS = [0.5, -0.1, 0.91]
+CS1_OFFSET = [0.15, -0.06, 0.02]
+Y_CART_STEP_SIZE = -0.04
+X_CART_STEP_SIZE = 0.04
+MAZE_ORIGIN_OFFSET = np.array(MAZE_POS) + np.array(CS1_OFFSET)
+
+# Actions lookup table up right down left
+ACTION_X_STEP = [0, 1, 0, -1]
+ACTION_Y_STEP = [1, 0, -1, 0]
+NUM_ACTIONS = 4
+CONSTRAINT = [0, 1]  # 0 for CLOCKWISE, 1 for COUNTERCLOCKWISE
+G = nx.DiGraph()
+attrs = {}
+lastConstraint = 0
+
+duration = 0.1
+maze = PyBulletObject(urdf_name='maze',
+                      object_name='maze',
+                      position=MAZE_POS,
+                      orientation=[0, 0, 0],
+                      data_dir=None)
+
+stick_pos = list(map(sum, zip(MAZE_POS, CS1_OFFSET)))
+stick = PyBulletObject(urdf_name='stick',
+                       object_name='stick',
+                       position=stick_pos,
+                       orientation=[0, 0, 0],
+                       data_dir=None)
+desired_quat_1 = [0, 1, 0, 0]
+
+object_list = [maze, stick]
+scene = Scene(object_list=object_list)
+
+PyBulletRobot = Robot(p, scene, gravity_comp=True)
+PyBulletRobot.use_inv_dyn = False
 
 
 def getCartPosFromIndex(x, y):
-    return maze_origin_offset + np.array([x_step_size * x, y_step_size * y, 0.02])
+    return MAZE_ORIGIN_OFFSET + np.array([Y_CART_STEP_SIZE * y, X_CART_STEP_SIZE * x, 0.02])
 
 
-def main():
-    maze = PyBulletObject(urdf_name='maze',
-                          object_name='maze',
-                          position=maze_pos,
-                          orientation=[0, 0, 0],
-                          data_dir=None)
+def isWall(x, y):
+    return x < 0 or x > len(MAZE_GRID[0]) - 1 or \
+           y < 0 or y > len(MAZE_GRID) - 1 or \
+           MAZE_GRID[y][x] == 1
 
-    stick_pos = list(map(sum, zip(maze_pos, cs1_offset)))
-    stick = PyBulletObject(urdf_name='stick',
-                           object_name='stick',
-                           position=stick_pos,
-                           orientation=[0, 0, 0],
-                           data_dir=None)
 
-    object_list = [maze, stick]
-    scene = Scene(object_list=object_list)
+def getMeasurment():
+    res = []
+    for a in range(NUM_ACTIONS):
+        pos_after_x = robot_grid_pos[0] + ACTION_X_STEP[a]
+        pos_after_y = robot_grid_pos[1] + ACTION_Y_STEP[a]
+        if isWall(pos_after_x, pos_after_y):
+            res.append(0)
+        else:
+            res.append(1)
+    return res
 
-    PyBulletRobot = Robot(p, scene, gravity_comp=True)
-    PyBulletRobot.use_inv_dyn = False
 
-    init_pos = PyBulletRobot.current_c_pos
-    init_or = PyBulletRobot.current_c_quat
-    init_joint_pos = PyBulletRobot.current_j_pos
+def constraintFollowing(constraint):
+    global lastConstraint
+    if constraint == 0:
+        s = 1
+    elif constraint == 1:
+        s = -1
+    else:
+        return
 
-    PyBulletRobot.startLogging()
-    # duration = 4
-    duration = 1
+    a = lastConstraint
+    for i in range(0, NUM_ACTIONS):
+        if constraint == 0:
+            constraint_dir = (a - 1) % NUM_ACTIONS
+        else:
+            constraint_dir = (a + 1) % NUM_ACTIONS
+        pos_after_x = robot_grid_pos[0] + ACTION_X_STEP[a]
+        pos_after_y = robot_grid_pos[1] + ACTION_Y_STEP[a]
+        pos_constraint_x = pos_after_x + ACTION_X_STEP[constraint_dir]
+        pos_constraint_y = pos_after_y + ACTION_Y_STEP[constraint_dir]
+        if isWall(pos_after_x, pos_after_y) or not isWall(pos_constraint_x, pos_constraint_y):
+            a = (a + s) % NUM_ACTIONS
+            continue
+        else:
+            lastConstraint = (a - s) % NUM_ACTIONS
+            robot_grid_pos[0] = pos_after_x
+            robot_grid_pos[1] = pos_after_y
+            PyBulletRobot.gotoCartPositionAndQuat(desiredPos=getCartPosFromIndex(robot_grid_pos[0], robot_grid_pos[1]),
+                                                  desiredQuat=desired_quat_1,
+                                                  duration=duration)
+            return keepMoingInDirection(a, constraint)
+
+
+def keepMoingInDirection(direction, constraint):
+    if constraint == 0:
+        constraint_dir = (direction - 1) % NUM_ACTIONS
+    else:
+        constraint_dir = (direction + 1) % NUM_ACTIONS
+    pos_after_x = robot_grid_pos[0] + ACTION_X_STEP[direction]
+    pos_after_y = robot_grid_pos[1] + ACTION_Y_STEP[direction]
+    pos_constraint_x = robot_grid_pos[0] + ACTION_X_STEP[constraint_dir]
+    pos_constraint_y = robot_grid_pos[1] + ACTION_Y_STEP[constraint_dir]
+    while not isWall(pos_after_x, pos_after_y) and isWall(pos_constraint_x, pos_constraint_y):
+        robot_grid_pos[0] = pos_after_x
+        robot_grid_pos[1] = pos_after_y
+        PyBulletRobot.gotoCartPositionAndQuat(desiredPos=getCartPosFromIndex(robot_grid_pos[0], robot_grid_pos[1]),
+                                              desiredQuat=desired_quat_1,
+                                              duration=duration)
+        pos_after_x = robot_grid_pos[0] + ACTION_X_STEP[direction]
+        pos_after_y = robot_grid_pos[1] + ACTION_Y_STEP[direction]
+        pos_constraint_x = robot_grid_pos[0] + ACTION_X_STEP[constraint_dir]
+        pos_constraint_y = robot_grid_pos[1] + ACTION_Y_STEP[constraint_dir]
+    return True
+
+
+def dummyHash(pos, measurement):
+    return ''.join(str(e) for e in pos + measurement)
+
+
+def slam():
+    measurement = getMeasurment()
+    startNode = dummyHash(robot_grid_pos, measurement)
+    attrs[startNode] = {"pos": robot_grid_pos, "measurement": measurement}
+    G.add_node(startNode)
+
+    lastNode = startNode
+    # Find all node
+    while 1:
+        if constraintFollowing(0):
+            measurement = getMeasurment()
+            curNode = dummyHash(robot_grid_pos, measurement)
+            if curNode in attrs:
+                break
+            else:
+                attrs[curNode] = {"pos": robot_grid_pos, "measurement": measurement}
+                G.add_node(curNode)
+                G.add_edge(lastNode, curNode)
+                lastNode = curNode
+    print("success!")
+
+
+# Correct edges
+# Add constrain direction
+
+
+def initRobot():
+    # init_pos = PyBulletRobot.current_c_pos
+    # init_or = PyBulletRobot.current_c_quat
+    # init_joint_pos = PyBulletRobot.current_j_pos
 
     PyBulletRobot.ctrl_duration = duration
     PyBulletRobot.set_gripper_width = 0.04
@@ -66,39 +177,15 @@ def main():
     desired_cart_pos_2 = desired_cart_pos_1 + np.array([0., 0, 0.02])
     PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_2, desiredQuat=desired_quat_1, duration=duration)
 
-    desired_cart_pos_3 = getCartPosFromIndex(1, 0)
+    desired_cart_pos_3 = getCartPosFromIndex(robot_grid_pos[0], robot_grid_pos[1])
     PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_3, desiredQuat=desired_quat_1, duration=duration)
 
-    desired_cart_pos_4 = getCartPosFromIndex(2, 0)
-    PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_4, desiredQuat=desired_quat_1, duration=duration)
 
-    desired_cart_pos_5 = getCartPosFromIndex(3, 0)
-    PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_5, desiredQuat=desired_quat_1, duration=duration)
+def main():
+    PyBulletRobot.startLogging()
 
-    desired_cart_pos_5 = getCartPosFromIndex(3, 1)
-    PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_5, desiredQuat=desired_quat_1, duration=duration)
-
-    desired_cart_pos_5 = getCartPosFromIndex(2, 1)
-    PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_5, desiredQuat=desired_quat_1, duration=duration)
-
-    desired_cart_pos_5 = getCartPosFromIndex(2, 2)
-    PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_5, desiredQuat=desired_quat_1, duration=duration)
-
-    desired_cart_pos_5 = getCartPosFromIndex(2, 3)
-    PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_5, desiredQuat=desired_quat_1, duration=duration)
-
-    desired_cart_pos_5 = getCartPosFromIndex(1, 3)
-    PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_5, desiredQuat=desired_quat_1, duration=duration)
-
-    desired_cart_pos_5 = getCartPosFromIndex(0, 3)
-    PyBulletRobot.gotoCartPositionAndQuat(desiredPos=desired_cart_pos_5, desiredQuat=desired_quat_1, duration=duration)
-
-    # get camera image
-    robot_id = PyBulletRobot.robot_id
-    client_id = scene.physics_client_id
-    scene.inhand_cam.get_image(cam_id=robot_id, client_id=client_id)
-    # scene.cage_cam.get_image(cam_id=robot_id, client_id=client_id)
-    print("show camera image")
+    initRobot()
+    slam()
 
     PyBulletRobot.stopLogging()
 
